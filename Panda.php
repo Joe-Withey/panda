2c1a3c2753b4b5ced6b2cff0435e6d08f16e5a02
Joe Withey <joseph.a.withey@gmail.com>
2017-08-17 08:54:35 +0100

    Changed indent to 4; added new functions

--- Panda.php
+++ Panda.php
@@ -2,115 +2,144 @@
 
 class P
 {
-  public static function pipe($array) {
-    return function ($init) use ($array) {
-      return self::reduce(function ($a, &$c) {
-        return $c($a);
-      }, $init, $array);
-    };
-  }
+    public static function pipe($array) {
+        return function ($init) use ($array) {
+            $fn = function ($a, $c) {
+                return $c($a);
+            };
 
-  public static function comp(&$a, &$b)
-  {
-    return function ($init) use (&$a, &$b) {
-      return $a($b($init));
-    };
-  }
+            return self::reduce($fn, $init, $array);
+        };
+    }
 
-  public static function reduce(&$cb = null, $init = null, $array = null) {
-    $argLength = 3;
-    $suppliedArgs = func_get_args();
+    public static function comp(&$a, &$b)
+    {
+        return function ($init) use (&$a, &$b) {
+            return $a($b($init));
+        };
+    }
 
-    $fn = function(&$cb, $init, $array) {
-      $i = 0;
-      $a = $init;
+    public static function reduce($cb, $init = null, $array = null) {
+        $argLength = 3;
+        $suppliedArgs = func_get_args();
 
-      while ($i < count($array)) {
-        $a = $cb($a, $array[$i++]);
-      }
+        $fn = function ($cb, $init, $array) {
+            $a = $init;
 
-      return $a;
-    };
+            foreach($array as $k => $v) {
+                $a = $cb($a, $v, $k);
+            }
 
-    return self::callOrDelay($argLength, $suppliedArgs, $fn);
-  }
+            return $a;
+        };
 
-  public static function map(&$cb = null, $array = null)
-  {
-    $argLength = 2;
-    $suppliedArgs = func_get_args();
+        return self::callOrDelay($argLength, $suppliedArgs, $fn);
+    }
 
-    $fn = function (&$cb, $array) {
-      $out = [];
+    public static function map($cb, $array = null)
+    {
+        $argLength = 2;
+        $suppliedArgs = func_get_args();
 
-      foreach($array as $k => $v) {
-        $out[$k] = $cb($v, $k);
-      }
+        $fn = function ($cb, $array) {
+            $out = [];
 
-      return $out;
-    };
+            foreach($array as $k => $v) {
+                $out[$k] = $cb($v, $k);
+            }
 
-    return self::callOrDelay($argLength, $suppliedArgs, $fn);
-  }
+            return $out;
+        };
 
-  public static function propEq($prop, $value = null, $array = null)
-  {
-    $argLength = 3;
-    $suppliedArgs = func_get_args();
+        return self::callOrDelay($argLength, $suppliedArgs, $fn);
+    }
 
-    $fn = function ($prop, $value, $array) {
-      return self::prop($prop, $array) === $value;
-    };
+    public static function filter($cb, $array = null)
+    {
+        $argLength = 2;
+        $suppliedArgs = func_get_args();
 
-    return self::callOrDelay($argLength, $suppliedArgs, $fn);
-  }
+        $fn = function ($cb, $array) {
+            $out = [];
 
-  public static function path($path, $array) 
-  {
-    $argLength = 2;
-    $suppliedArgs = func_get_args();
+            foreach($array as $k => $v) {
+                if ($cb($v, $k)) {
+                    $out[$k] = $v;
+                }
+            }
 
-    $fn = function($path, $array) {
-      return self::reduce();
-    };
+            return $out;
+        };
 
-    return self::callOrDelay($argLength, $suppliedArgs, $fn);
-  }
+        return self::callOrDelay($argLength, $suppliedArgs, $fn);
+    }
 
-  public static function flip($fn)
-  {
-    return function($a, $b) {
-      return $fn($b, $a);
-    };
-  }
+    public static function propEq($prop, $value = null, $array = null)
+    {
+        $argLength = 3;
+        $suppliedArgs = func_get_args();
 
-  public static function prop($prop, $array = null)
-  {
-    $argLength = 2;
-    $suppliedArgs = func_get_args();
+        $fn = function ($prop, $value, $array) {
+            return self::prop($prop, $array) === $value;
+        };
 
-    $fn = function($prop, $array) {
-      return $array[$prop];
-    };
+        return self::callOrDelay($argLength, $suppliedArgs, $fn);
+    }
 
-    return self::callOrDelay($argLength, $suppliedArgs, $fn);
-  }
+    public static function path($path, $array)
+    {
+        $argLength = 2;
+        $suppliedArgs = func_get_args();
 
-  private static function callOrDelay($argLength, &$suppliedArgs, &$fn)
-  {
-    $closure = function () use ($argLength, &$suppliedArgs, &$fn, &$closure) {
-      $args = func_get_args();
+        $fn = function($path, $array) {
+            return self::reduce(function ($a, $c) {
+                return self::prop($c, $a);
+            }, $array, $path);
+        };
 
-      $suppliedArgs = array_merge($suppliedArgs, $args);
+        return self::callOrDelay($argLength, $suppliedArgs, $fn);
+    }
 
-      return count($suppliedArgs) === $argLength ?
-        call_user_func_array($fn, $suppliedArgs) :
-        $closure;
-    };
+    public static function flip($fn)
+    {
+        return function($a, $b) {
+            return $fn($b, $a);
+        };
+    }
 
-    return count($suppliedArgs) === $argLength ?
-      call_user_func_array($fn, $suppliedArgs) :
-      $closure;
-  }
+    public static function prop($prop, $array = null)
+    {
+        $argLength = 2;
+        $suppliedArgs = func_get_args();
+
+        $fn = function($prop, $array) {
+            if (is_null($a) || !isset($a[$c])) {
+                return null;
+            }
+
+            return $array[$prop];
+        };
+
+        return self::callOrDelay($argLength, $suppliedArgs, $fn);
+    }
+
+    private static function callOrDelay($argLength, &$suppliedArgs, &$fn)
+    {
+        if (count($suppliedArgs) === $argLength) {
+            return call_user_func_array($fn, $suppliedArgs);
+        }
+
+        $closure = function () use ($argLength, &$suppliedArgs, &$fn, &$closure) {
+            $args = func_get_args();
+
+            $suppliedArgs = array_merge($suppliedArgs, $args);
+
+            return count($suppliedArgs) === $argLength ?
+                call_user_func_array($fn, $suppliedArgs) :
+                $closure;
+        };
+
+        return $closure;
+    }
 }
 
